3aa2736313c9cca5b43426f576fdd958444864c6
sukanya004sinha <sukanyasinha004@gmail.com>
2023-05-13 14:39:32 +0530

    initial commit upto products

---
+++ src/Products/ProductsList.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface ProductsListProps {
+}
+const initialProducts = {
+    
+}
+    const ProductsList: React.FC<ProductsListProps> = ({})=> {
+        return(
+            <div className='App'>
+       <label>Games</label>
+            </div>
+        );
+
+    }
+    export default ProductsList;
